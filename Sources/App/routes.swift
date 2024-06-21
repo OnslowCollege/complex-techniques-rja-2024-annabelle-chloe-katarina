4004537f55b9b67d2a568fbcdc7f963342d37ea9
Katskiiii <100874682+Katskiiii@users.noreply.github.com>
2024-06-21 01:46:04 +0000

    Jun 21, 2024, 1:46 AM

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,18 +2,9 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    // Landing page
-    app.get { req -> EventLoopFuture<View> in
-        return req.view.render("index")
-    }
-
-    // Product page for eggs
-    app.get("eggs") { req -> EventLoopFuture<View> in
-        return req.view.render("eggs")
-    }
-    
-    // Cart page
-    app.get("cart") { req -> EventLoopFuture<View> in
-        return req.view.render("cart")
+    // Route for the login page
+    app.get("login") { req -> EventLoopFuture<View> in
+        return req.view.render("login")
     }
 }
+
