d4cbad82997279affa6c6d62a189b5dbeeaa1f5d
Katskiiii <100874682+Katskiiii@users.noreply.github.com>
2024-06-24 00:06:57 +0000

    Jun 24, 2024, 12:06 AM

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,8 +6,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
 app.databases.use(DatabaseConfigurationFactory.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
