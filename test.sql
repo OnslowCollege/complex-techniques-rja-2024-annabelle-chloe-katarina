12518aa839b3584861939513dc6c537965468638
Chloe <124315046+chloesypena@users.noreply.github.com>
2024-06-25 09:24:24 +0000

    Jun 25, 2024, 9:24 AM

--- test.sql
+++ test.sql
@@ -1,12 +1,11 @@
 -- Create egg shop database.
 CREATE DATABASE egg_shop;
+
 -- Create table 'user_2'.
-CREATE TABLE user_2 (name varchar(50));
+CREATE TABLE user_2 (user_id INT AUTO_INCREMENT UNSIGNED PRIMARY KEY NOT NULL);
 
 -- Show all the databases created.
 SHOW DATABASES;
 
 USE egg_shop;
 SHOW TABLES;
-
-
