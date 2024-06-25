8f8df50e9e7679f64355cd65a8e9fbd665aaf50e
Chloe <124315046+chloesypena@users.noreply.github.com>
2024-06-25 09:18:02 +0000

    Jun 25, 2024, 9:18 AM

--- test.sql
+++ test.sql
@@ -1,7 +1,12 @@
-CREATE TABLE testtable (name varchar(50));
-
-SHOW DATABASES;
-
 -- Create egg shop database.
 CREATE DATABASE egg_shop;
+-- Create table 
+CREATE TABLE user_2 (name varchar(50));
+
+-- Show all the databases created.
+SHOW DATABASES;
+
+USE egg_shop;
+
+
 
