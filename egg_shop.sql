6fc85bf0d598deed086c0e9ed0ca10cf04b54931
Chloe <124315046+chloesypena@users.noreply.github.com>
2024-06-28 06:35:47 +0000

    Jun 28, 2024, 6:35 AM

--- egg_shop.sql
+++ egg_shop.sql
@@ -9,8 +9,6 @@
 --
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `user_id` int unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
